c10e84b740c989dee1701f3beff8e9f860a53062
Ryan Faircloth <ryan@dss-i.com>
2022-10-13 14:08:45 -0400

    chore: initial checkin

--- .github/workflows/scripts/prepareHelm.sh
+++ .github/workflows/scripts/prepareHelm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
 yq -i ".version = \"${1}\" | .appVersion style=\"double\" | .appVersion = \"${1}\"" chart/Chart.yaml
+helm repo add k8ss http://helm.yotron.de/
 helm dependency build chart
 helm package chart
